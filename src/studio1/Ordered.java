d8c730e865bed76877819ab6ddfe6db3395d65d3
maria <maria@MariaLaptop.dhcp.wustl.edu>
2023-09-06 13:55:47 -0500

    Studio 01 completed

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,10 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		
+		boolean great = (x < y << z);
+		boolean less = (x > y >> z);
+		String isOrdered = (great || less) ? "ordered" : "not ordered";
+		System.out.print(isOrdered);
 	}
-
 }
